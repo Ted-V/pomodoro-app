4ee9fcdf765180a2c9e70883c1371eb2f2ab3252
Ted V <87731571+Ted-V@users.noreply.github.com>
2023-06-24 01:02:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
+// Timer Class
 class PomodoroTimer {
     constructor() {
-        this.timer = null;
-        this.minutes = 25;
-        this.seconds = 0;
-        this.isRunning = false;
-        this.isBreak = false;
-        this.counter = 0;
+        this.timer = null; // Timer reference
+        this.minutes = 25; // Initial minutes
+        this.seconds = 0; // Initial seconds
+        this.isRunning = false; // Timer running flag
+        this.isBreak = false; // Break mode flag
+        this.counter = 0; // Completed pomodoros counter
     }
 
+    // Start or pause the timer
     startTimer() {
         if (!this.isRunning) {
+            // Start the timer
             this.isRunning = true;
             document.getElementById("start-button").innerHTML = "Pause";
             document.getElementById("reset-button").disabled = false;
             this.timer = setInterval(() => this.updateTimer(), 1000);
         } else {
+            // Pause the timer
             this.isRunning = false;
             document.getElementById("start-button").innerHTML = "Resume";
             clearInterval(this.timer);
         }
     }
 
+    // Reset the timer
     resetTimer() {
         clearInterval(this.timer);
         this.isRunning = false;
@@ -32,68 +37,77 @@ class PomodoroTimer {
         this.updateTimerDisplay();
     }
 
+    // Update the timer
     updateTimer() {
         if (this.seconds > 0) {
+            // Decrease seconds
             this.seconds--;
         } else if (this.minutes > 0) {
+            // Decrease minutes and reset seconds
             this.minutes--;
             this.seconds = 59;
         } else {
+            // Timer completed
             clearInterval(this.timer);
             this.isRunning = false;
             document.getElementById("start-button").innerHTML = "Start";
             document.getElementById("reset-button").disabled = true;
 
             if (!this.isBreak) {
+                // Start break
                 this.minutes = 5;
-                this.seconds = 0;
                 this.isBreak = true;
-                this.playAlarmSound();
-                this.showNotification();
             } else {
+                // Start next pomodoro session
                 this.minutes = 25;
-                this.seconds = 0;
                 this.isBreak = false;
                 this.counter++;
                 this.updateSessionCounter();
+                this.playAlarmSound();
+                this.showNotification();
             }
         }
 
         this.updateTimerDisplay();
     }
 
+    // Update the timer display
     updateTimerDisplay() {
         const minutes = this.formatTime(this.minutes);
         const seconds = this.formatTime(this.seconds);
         document.getElementById("time").innerHTML = `${minutes}:${seconds}`;
     }
 
+    // Update the session counter
     updateSessionCounter() {
         const sessionCounter = document.getElementById("session-counter");
         sessionCounter.innerHTML = `Completed Pomodoros: ${this.counter}`;
     }
 
+    // Format time with leading zero
     formatTime(time) {
         return time < 10 ? "0" + time : time;
     }
 
+    // Play alarm sound
     playAlarmSound() {
         const audio = new Audio("alarm.wav");
         audio.play();
     }
 
+    // Show browser notification
     showNotification() {
         if (Notification.permission === "granted") {
             new Notification("Pomodoro Timer", {
-                body: "Pomodoro session completed! Take a break.",
-                icon: "tomato.png"
+                body: "Pomodoro session completed!",
+                icon: "tomato.png",
             });
         } else if (Notification.permission !== "denied") {
             Notification.requestPermission().then(function (permission) {
                 if (permission === "granted") {
                     new Notification("Pomodoro Timer", {
-                        body: "Pomodoro session completed! Take a break.",
-                        icon: "tomato.png"
+                        body: "Pomodoro session completed!",
+                        icon: "tomato.png",
                     });
                 }
             });
@@ -101,4 +115,18 @@ class PomodoroTimer {
     }
 }
 
+// Create Pomodoro Timer instance
 const pomodoroTimer = new PomodoroTimer();
+
+document.getElementById("start-button").addEventListener("click", () => {
+    pomodoroTimer.startTimer();
+});
+
+document.getElementById("reset-button").addEventListener("click", () => {
+    pomodoroTimer.resetTimer();
+});
+
+document.getElementById("mode-toggle-button").addEventListener("click", () => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+});
